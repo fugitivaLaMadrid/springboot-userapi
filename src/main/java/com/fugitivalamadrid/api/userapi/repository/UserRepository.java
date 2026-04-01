654d880c69096799249a7b3044bca155b713cead
fugitiva <saralamadrid@outlook.com>
2026-04-01 16:59:04 +0200

    feat(api): Remove unused method in UserRepository

--- src/main/java/com/fugitivalamadrid/api/userapi/repository/UserRepository.java
+++ src/main/java/com/fugitivalamadrid/api/userapi/repository/UserRepository.java
@@ -3,9 +3,6 @@ package com.fugitivalamadrid.api.userapi.repository;
 import com.fugitivalamadrid.api.userapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
  //because we extends JpaRepository we can get, update,find, save, delete all users
-    List<User> findByUsernameContainingIgnoreCase(String username);
 }
